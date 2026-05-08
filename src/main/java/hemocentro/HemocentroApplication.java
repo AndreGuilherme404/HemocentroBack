888920b404c8b6f50312358c4318f93ed9c45504
unknown <ag33344431@gmail.com>
2026-05-08 09:17:11 -0300

    Hello World

---
+++ src/main/java/hemocentro/HemocentroApplication.java
@@ -0,0 +1,13 @@
+package hemocentro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HemocentroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HemocentroApplication.class, args);
+	}
+
+}
